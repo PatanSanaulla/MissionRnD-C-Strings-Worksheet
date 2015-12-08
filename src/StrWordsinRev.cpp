47e9a2f6f87b8be4c7e3e1d7f2b327a6b042bdbc
Sanaulla khan <spatankhan@gmail.com>
2015-12-09 01:59:25 +0530

    Submit Commit

--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -13,6 +13,43 @@ NOTES: Don't create new string.
 #include <string.h>
 
 void str_words_in_rev(char *input, int len){
+	int i = 0,word, strt=0;
+	char c;
+	for (i = 0; i < len;i++)
+	{
+		if (input[i] == ' ')
+		{
+			word = i - 1;
+			while (word >= strt)
+			{
+				c = input[strt];
+				input[strt] = input[word];
+				input[word] = c;
+				word--;
+				strt++;
+			}
+			strt = i + 1;
+		}
+		//i++;
+	}
+	word = len - 1;
+	while (word >= strt)
+	{
+		c = input[strt];
+		input[strt] = input[word];
+		input[word] = c;
+		word--;
+		strt++;
+	}
+	word = len - 1;
+	strt = 0;
+	while (strt <= word)
+	{
+		c = input[strt];
+		input[strt] = input[word];
+		input[word] = c;
+		word--;
+		strt++;
+	}
 
-	
 }
